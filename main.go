ce2e1aa345cf19966377ab79e23eb13025b875fe
markustenghamn <markus@tenghamn.com>
2019-05-20 12:39:27 +0200

    various improvements

--- main.go
+++ main.go
@@ -10,19 +10,33 @@ import (
 	"strconv"
 )
 
+type Message int
+
+const (
+	Positive Message = 0
+	Negative Message = 1
+	Neutral  Message = 2
+)
+
+type Customer struct {
+	Replied     bool
+	Done        bool
+	MessageType Message
+}
+
 var port = ":80"
 
 var numberFrom = ""
 var username = ""
 var password = ""
+var slackWebhook = ""
 
-// I use two bools since I only run this for one phone number, if you have multiple phone numbers I would change this to a map like map[phone]replied or something similar
-var replied = false
-var done = false
+// A map of customers so we can handle multiple phone numbers/customers
+var customers map[string]Customer
 
 var message1 = "Hello Markus, thank you for visiting GoPHP.io! On a scale from 1 to 10, with 10 being the best, how would you rate your experience?"
-var messagepositive = "Thank you for your feedback, we are happy you had a good experience at GoPHP.io! Please reply back to us if you would like to tell us why you liked the experience"
-var messagenegative = "We are sorry that you did not enjoy visiting GoPHP.io. If there is anything we can do to make the experience better please do not hesitate to contact us at markus@gophp.io."
+var messagepositive = "We are happy you had a good experience at GoPHP.io! Please reply back to us if you would like to tell us why you liked the experience"
+var messagenegative = "We are sorry GoPHP.io did not live up to your expectations. Please reply back to us if you would like to tell us why you were not satisfied"
 var messagefinal = "Thank you for your feedback, if you would like to get in touch with us please send an email to markus@gophp.io."
 
 func main() {
@@ -30,13 +44,14 @@ func main() {
 	args := os.Args[1:]
 
 	// Error if not enough arguments
-	if len(args) != 3 {
-		panic("must supply 46elks username and password and from number")
+	if len(args) != 4 {
+		panic("must supply 46elks username and password, from number and a Slack webhook")
 	}
 
 	username = args[0]
 	password = args[1]
 	numberFrom = args[2]
+	slackWebhook = args[3]
 
 	// This sets up two endpoints http://localhost:8080/outgoing and http://localhost:8080/incoming
 	// I recommend running this program behind something like Caddy (https://caddyserver.com/) that provides SSL and can proxy to the localhost
@@ -49,6 +64,7 @@ func main() {
 	}
 }
 
+// handleOutgoingSMS parses the field "to" from a post request and passes it to the sendSMS function
 func handleOutgoingSMS(w http.ResponseWriter, r *http.Request) {
 
 	if err := r.ParseForm(); err != nil {
@@ -56,14 +72,15 @@ func handleOutgoingSMS(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Direction is 'incoming'
 	to := r.FormValue("to")
 
 	sendSMS(to, message1)
 
 }
 
+// handleIncomingSMS receives requests from the 46elks API when a new text message is received
 func handleIncomingSMS(w http.ResponseWriter, r *http.Request) {
+	var customer Customer
 	// According to the documentation we can expect incoming messages to come in the following format as a post request in application/x-www-form-urlencoded
 	//
 	// direction=incoming&
@@ -92,39 +109,49 @@ func handleIncomingSMS(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("[%s][%s][%s] %s => %s: %s\n", created, direction, id, from, to, message)
 
-	if !replied && !done {
-		// If the message is less than 2 characters in length we try to convert to int
-		if len(message) <= 2 {
-			value, err := strconv.Atoi(message)
-			// If we successfully converted to an int we can process the message
-			if err == nil {
-				// I would say an 8 or higher is positive, 7 or lower would indicate something is wrong (just my assumption)
-				if value >= 8 {
-					sendSMS(from, messagepositive)
-					replied = true
-					return
-				} else {
-					// If we did not have a message with 8 or higher
-					sendSMS(from, messagenegative)
-					replied = true
-					return
-				}
-			}
+	// We check if the customer exists
+	if val, ok := customers[from]; ok {
+		customer = val
+	} else {
+		// If the customer does not exist it means we have not received a reply before
+		customer = Customer{Replied: false, Done: false, MessageType: Neutral}
+	}
+	if !customer.Replied && !customer.Done {
+		msgType := determineMessageType(message)
+		// I would say an 8 or higher is positive, 7 or lower would indicate something is wrong (just my assumption)
+		if msgType == Positive {
+			sendSMS(from, messagepositive)
+			customer.MessageType = msgType
+			customer.Replied = true
+			customers[from] = customer
+			return
+		} else if msgType == Negative {
+			// If we did not have a message with 8 or higher
+			sendSMS(from, messagenegative)
+			customer.MessageType = msgType
+			customer.Replied = true
+			customers[from] = customer
+			return
 		}
 		// If we are unable to determine a positive or negative answer
 		// In a live environment I would recommend saving these longer responses and maybe sending it to a support desk to manually handle them
 		sendSMS(from, messagefinal)
-		replied = true
-		done = true
+		customer.Replied = true
+		customer.Done = true
+		customers[from] = customer
+		updateSlack(from, message, customer.MessageType)
 		return
 
-	} else if !done {
+	} else if !customer.Done {
 		sendSMS(from, messagefinal)
-		done = true
+		customer.Done = true
+		customers[from] = customer
+		updateSlack(from, message, customer.MessageType)
 		return
 	}
 }
 
+// sendSMS sends a post request to 46elks which then tries to send a text message to the phone number provided as the "to" string
 func sendSMS(to string, message string) {
 	data := url.Values{
 		"from":    {numberFrom},
@@ -154,5 +181,75 @@ func sendSMS(to string, message string) {
 	}
 
 	fmt.Printf("Sent sms to %s: %s\n", to, message)
+	fmt.Println(string(body))
+}
+
+func determineMessageType(message string) Message {
+	// If the message is less than 2 characters in length we try to convert to int
+	if len(message) <= 2 {
+		value, err := strconv.Atoi(message)
+		// If we successfully converted to an int we can process the message
+		if err == nil {
+			// I would say an 8 or higher is positive, 7 or lower would indicate something is wrong (just my assumption)
+			if value >= 8 {
+				return Positive
+			} else if value <= 3 {
+				// If we did not have a message with 8 or higher
+				return Negative
+
+			}
+		}
+	}
+	// If we are unable to determine a positive or negative answer
+	return Neutral
+}
+
+func updateSlack(from string, message string, t Message) {
+	// I like to color code the replies, here we have red for negative, green for positive and gray for neutral
+	color := "#C0C0C0"
+	if t == Positive {
+		color = "#006400"
+	} else if t == Negative {
+		color = "#8B0000"
+	}
+	var jsonStr = []byte(`{
+			"attachments": [
+				{
+					"fallback": "` + message + `",
+					"color": "` + color + `",
+					"text": "` + message + `",
+					"fields": [
+						{
+							"title": "Tel",
+							"value": "` + from + `",
+							"short": false
+						}
+					],
+				}
+    		]}`)
+	// See the Slack documentation on creating webhooks https://api.slack.com/incoming-webhooks
+	req, err := http.NewRequest("POST", slackWebhook, bytes.NewBuffer(jsonStr))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Length", strconv.Itoa(len(jsonStr)))
+	req.SetBasicAuth(username, password)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Sent slack message: %s - %s\n", from, message)
 	fmt.Println(string(body))
 }
--- readme.md
+++ readme.md
@@ -2,6 +2,15 @@
 
 An example of a customer feedback bot to rate and get text input regarding past sales via text messaging written in go.
 
-Run like so: `go run main.go [46elks username] [46elks password] [Elks46 phone number]`
+Run like so: `go run main.go [46elks username] [46elks password] [Elks46 phone number] [Slack webhook]`
 
 You can register for a 46elks account on their [website](https://46elks.com).
+
+To send an email just send a post request with a `to` field to `/outgoing`, below is an example in curl
+
+```
+curl -X POST \
+  http://<your url here>/outgoing \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -d 'to=<your number here>'
+```
